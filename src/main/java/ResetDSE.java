16535624faef0c71ff6201d46497ab866b73bd17
sequoyha <sequoyha@gmail.com>
2016-09-02 09:34:48 -0700

    Cleaned up GetLow Error msg, added resetDse

--- src/main/java/ResetDSE.java
+++ src/main/java/ResetDSE.java
@@ -1,20 +1,18 @@
-
-
 /**
- * Created by sequoyha on 9/1/16.
+ * Created by sequoyha on 9/2/16.
  */
-public class GetLow
+public class ResetDSE
 {
-    public void fuckUpGarbageCollector()
+    public void performReset()
     {
         try
         {
-            String[] cmd = {"/bin/bash","-c","echo password| sudo -S /home/support/gc-labs/bash/gc_config_changes"};
+            String[] cmd = {"/bin/bash","-c","echo password| sudo -S /home/support/gc-labs/bash/resetDse"};
             Process pb = Runtime.getRuntime().exec(cmd);
         }catch (Exception e)
         {
             System.out.print("Failure in execution\n" + e.getMessage() +"\n" + e.getStackTrace().toString());
         }
-
     }
+
 }
